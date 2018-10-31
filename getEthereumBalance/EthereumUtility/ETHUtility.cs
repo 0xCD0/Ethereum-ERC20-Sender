574405ed1c336e5c5333cd01f310b56d720cea46
64bitm <64bitm@gmail.com>
2018-10-31 20:30:11 +0900

    ERC-20 Transfer / Balance Query Added
    ERC-20 Transfer / Balance Query Added

--- getEthereumBalance/EthereumUtility/ETHUtility.cs
+++ getEthereumBalance/EthereumUtility/ETHUtility.cs
@@ -3,6 +3,11 @@ using System.Threading.Tasks;
 using Nethereum.Web3;
 using Nethereum.Web3.Accounts;
 using Nethereum.Hex.HexTypes;
+using Nethereum.ABI.FunctionEncoding.Attributes;
+using Nethereum.Contracts.CQS;
+using System.Numerics;
+using Nethereum.StandardTokenEIP20.CQS;
+using Nethereum.Util;
 
 /// <summary>
 /// Nethereum을 손쉽게 사용하기 위한 함수들이 모인 Utility 클래스입니다.
@@ -24,12 +29,18 @@ namespace EthereumUtility.ETHUtility {
         #endregion
 
         #region 로컬 변수
+        // 사용자의 Private Key 입니다. 반드시 입력하여 주십시오.
         public string PrivateKey { get; set; }
 
+        // ERC-20의 고유 Smart Contract Address 입니다. ERC-20 토큰을 보낼 때 반드시 입력하여 주십시오.
+        public string SmartContractAddress { get; set; }
+
+        // RPC 서버를 선택합니다. 기본 값은 MainNet으로 되어있습니다.
         public EthereumServerList Server = EthereumServerList.MainNet_Ethereum_Network;
         #endregion
 
         #region 함수
+        #region Ethereum 함수
         /// <summary>
         /// 로컬 변수인 this.PrivateKey에 등록된 PrivateKey를 기반으로 Ethereum Wallet의 잔액을 조회합니다.
         /// </summary>
@@ -49,7 +60,7 @@ namespace EthereumUtility.ETHUtility {
                 #endregion
             }
             catch (Exception ex) {
-                Console.WriteLine($"Ethereum Wallet 주소가 잘못되었거나 잘못된 입력입니다. \r\n\r\n Original Message : { ex.ToString()}");
+                Console.WriteLine($"Error Occured ! \r\n Original Message : { ex.ToString()}");
             }
 
         }
@@ -57,8 +68,7 @@ namespace EthereumUtility.ETHUtility {
         /// <summary>
         /// 인자로 입력하는 WalletAddress를 기반으로 Ethereum Wallet 잔액을 조회합니다.
         /// </summary>
-        /// <param name="WalletAddress"></param>
-        /// <returns></returns>
+        /// <param name="WalletAddress">Ethereum Wallet 주소를 입력합니다.</param>
         public async Task GetAccountBalanceFromTargetWallet(string WalletAddress) {
             string serverLocation = GetServerLocationFormEnum(Server);
 
@@ -75,9 +85,8 @@ namespace EthereumUtility.ETHUtility {
                 #endregion
             }
             catch (Exception ex) {
-                Console.WriteLine($"Ethereum Wallet 주소가 잘못되었거나 잘못된 입력입니다. \r\n\r\n Original Message : { ex.ToString()}");
+                Console.WriteLine($"Error Occured ! \r\n Original Message : { ex.ToString()}");
             }
-
         }
 
         /// <summary>
@@ -86,7 +95,6 @@ namespace EthereumUtility.ETHUtility {
         /// <param name="ToAddress">Ethereum을 수신 받을 지갑 주소를 입력합니다.</param>
         /// <param name="Balance">얼만큼의 Ethereum을 보낼 것인지 float형태로 입력합니다.</param>
         /// <param name="Decimal">Ethereum의 Decimal(자릿수) 입니다. 기본값은 0입니다.</param>
-        /// <returns></returns>
         public async Task TransferEthereum(string ToAddress, float Balance, int Decimal = 0) {
             string serverLocation = GetServerLocationFormEnum(Server);
 
@@ -101,10 +109,133 @@ namespace EthereumUtility.ETHUtility {
             }
 
             catch (Exception ex) {
-                Console.WriteLine($"Private Key가 잘못되었거나 서버가 응답하지 않습니다. \r\n\r\n Original Message : { ex.ToString()}");
+                Console.WriteLine($"Error Occured ! \r\n Original Message : { ex.ToString()}");
+            }
+        }
+        #endregion
+
+        #region ERC-20 Token 함수
+        /// <summary>
+        /// 로컬 변수인 this.PrivateKey에 등록된 PrivateKey를 기반으로 ERC-20 Token의 잔액을 조회합니다.
+        /// </summary>
+        public async Task GetAccountTokenBalanceFromPrivateKey() {
+            if (SmartContractAddress == string.Empty || SmartContractAddress == "") {
+                throw (new Exception("Token을 전송하려면 SmartContract 주소부터 입력해야합니다."));
             }
 
+            string serverLocation = GetServerLocationFormEnum(Server);
+
+            try {
+                var web3 = new Web3(serverLocation);
+
+                var tokenService = new Nethereum.StandardTokenEIP20.StandardTokenService(web3, SmartContractAddress);
+                var ownerBalanceTask = tokenService.GetBalanceOfAsync<BigInteger>(GetWalletAddressFromPrivateKey());
+                var Balance = await ownerBalanceTask;
+
+                Console.WriteLine(Balance.ToString());
+            }
+            catch (Exception ex) {
+                Console.WriteLine($"Error Occured ! \r\n Original Message : { ex.ToString()}");
+            }
         }
+
+        /// <summary>
+        /// 인자로 입력하는 WalletAddress를 기반으로 ERC-20 Token 잔액을 조회합니다.
+        /// </summary>
+        /// <param name="WalletAddress">Ethereum Wallet 주소를 입력합니다.</param>
+        /// <param name="ContractAddress">SmartContract 주소(Token의 계약 고유 주소)를 입력합니다.</param>
+        public async Task GetAccountTokenBalanceFromTargetWallet(string WalletAddress, string ContractAddress) {
+            if (SmartContractAddress == string.Empty || SmartContractAddress == "") {
+                throw (new Exception("Token을 전송하려면 SmartContract 주소부터 입력해야합니다."));
+            }
+
+            string serverLocation = GetServerLocationFormEnum(Server);
+
+            try {
+                var web3 = new Web3(serverLocation);
+
+                var tokenService = new Nethereum.StandardTokenEIP20.StandardTokenService(web3, ContractAddress);
+                var ownerBalanceTask = tokenService.GetBalanceOfAsync<BigInteger>(WalletAddress);
+                var Balance = await ownerBalanceTask;
+
+                Console.WriteLine(Balance.ToString());
+            }
+            catch (Exception ex) {
+                Console.WriteLine($"Error Occured ! \r\n Original Message : { ex.ToString()}");
+            }
+        }
+
+        /// <summary>
+        /// ERC-20 토큰을 전송합니다. 전송된 후에는 TransactionHashReady_Event 이벤트가 작동되어 Transaction Hash가 전달됩니다.
+        /// </summary>
+        /// <param name="ToAddress">ERC-20 Token을 수신 받을 Ethereum 지갑 주소를 입력합니다.</param>
+        /// <param name="Balance">얼만큼의 Token을 보낼 것인지 int 형태로 입력합니다.</param>
+        /// <returns></returns>
+        public async Task TransferTokens(string ToAddress, UInt64 Balance) {
+            string serverLocation = GetServerLocationFormEnum(Server);
+
+            try {
+                var account = new Account(PrivateKey);
+                var web3 = new Web3(account, serverLocation);
+
+                var transactionMessage = new TransferFunction() {
+                    Value = Balance,
+                    FromAddress = account.Address,
+                    To = ToAddress,
+                    GasPrice = Web3.Convert.ToWei(25, UnitConversion.EthUnit.Gwei)
+                };
+
+                var transferHandler = web3.Eth.GetContractTransactionHandler<TransferFunction>();
+
+                // 최소 필요 가스 용량 계산
+                var estimate = await transferHandler.EstimateGasAsync(transactionMessage, SmartContractAddress);
+                transactionMessage.Gas = estimate.Value;
+
+                var transactionHash = await transferHandler.SendRequestAsync(transactionMessage, SmartContractAddress);
+
+                TransactionHashReady_Event(transactionHash.ToString());
+            }
+            catch (Exception ex) {
+                Console.WriteLine($"Error Occured ! \r\n Original Message : { ex.ToString()}");
+            }
+        }
+
+        /// <summary>
+        /// ContractAddress를 별도로 입력하여 ERC-20 토큰을 전송합니다. 전송된 후에는 TransactionHashReady_Event 이벤트가 작동되어 Transaction Hash가 전달됩니다.
+        /// </summary>
+        /// <param name="ToAddress">ERC-20 Token을 수신 받을 Ethereum 지갑 주소를 입력합니다.</param>
+        /// <param name="Balance">얼만큼의 Token을 보낼 것인지 int 형태로 입력합니다. Decimal은 자동 계산되므로 보낼 수량의 절대값만 입력하면 됩니다.</param>
+        /// <param name="ContractAddress">SmartContract 주소(Token의 계약 고유 주소)를 입력합니다.</param>
+        /// <returns></returns>
+        public async Task TransferTokens(string ToAddress, UInt64 Balance, string ContractAddress) {
+            string serverLocation = GetServerLocationFormEnum(Server);
+
+            try {
+                var account = new Account(PrivateKey);
+                var web3 = new Web3(account, serverLocation);
+
+                var transactionMessage = new TransferFunction() {
+                    Value = (Balance * 100),
+                    FromAddress = account.Address,
+                    To = ToAddress,
+                    GasPrice = Web3.Convert.ToWei(25, UnitConversion.EthUnit.Gwei)
+                };
+
+                var transferHandler = web3.Eth.GetContractTransactionHandler<TransferFunction>();
+
+                // 최소 필요 가스 용량 계산
+                var estimate = await transferHandler.EstimateGasAsync(transactionMessage, ContractAddress);
+                transactionMessage.Gas = estimate.Value;
+
+                var transactionHash = await transferHandler.SendRequestAsync(transactionMessage, ContractAddress);
+
+                TransactionHashReady_Event(transactionHash.ToString());
+            }
+            catch (Exception ex) {
+                Console.WriteLine($"Error Occured ! \r\n Original Message : { ex.ToString()}");
+            }
+        }
+        #endregion
 
         #region 내부 유틸리티 함수
         /// <summary>
@@ -146,7 +277,6 @@ namespace EthereumUtility.ETHUtility {
             return _server;
         }
         #endregion
-
         #endregion
     }
 }
--- getEthereumBalance/Program.cs
+++ getEthereumBalance/Program.cs
@@ -8,7 +8,27 @@ namespace EthereumBalance {
         public static AccountUtility acc;
 
         static void Main(string[] args) {
-            #region 지갑 생성 코드
+            #region 기본 입력 코드
+            eth = new ETHUtility();
+
+            // 자신의 Ethereum Wallet의 Private Key를 입력
+            eth.PrivateKey = "Input Your ethereum wallet private key";
+
+            // ERC-20 Smart Contract Address 주소 입력 (ERC-20 토큰을 사용하지 않을 것이라면 입력하지 않아도 무관)
+            eth.SmartContractAddress = "Input smart contract address";
+
+            // RPC 서버 선택
+            eth.Server = ETHUtility.EthereumServerList.Ropsten_Test_Network;
+
+            // 보낼 상대방 지갑 주소
+            string toAddr = "Input receiver ethereum wallet address";
+
+            // Ethereum 전송 완료 후 Transaction Hash를 받기 위한 Callback 등록
+            eth.TransactionHashReady_Event += Eth_TransactionHashReady_Event;
+
+            #endregion
+
+            #region Ethereum 지갑 생성 코드
             // Ethereum의 지갑 생성 코드입니다. 아래 주석을 풀고 실행 후 생성 성공시 PrivateKey와 WalletAccount 주소가 출력됩니다.
             // WalletAddress 주소는 랜덤 Hash 입니다.
 
@@ -18,28 +38,31 @@ namespace EthereumBalance {
             //Console.WriteLine($"Private Key : {temp.privateKey.ToString()} \r\nWalletAddress : {temp.accountAddress.ToString()}");
             #endregion
 
-            #region Sample Code
-            eth = new ETHUtility();
-
-            // Ethereum 전송 완료 후 Transaction Hash를 받기 위한 Callback 등록
-            eth.TransactionHashReady_Event += Eth_TransactionHashReady_Event;
-
-            // 자신의 Ethereum Wallet의 Private Key를 입력
-            eth.PrivateKey = "Input Your ethereum wallet private key";
-
-            // RPC 서버 선택
-            eth.Server = ETHUtility.EthereumServerList.Ropsten_Test_Network;
-
+            #region Ethereum 전송 및 조회 샘플 코드
             // 잔액 조회 
             eth.GetAccountBalanceFromPrivateKey().Wait();
 
-            // Ethereum 송금 (toAddr에 송금할 Ethereum Wallet 주소 입력)
-            string toAddr = "Input receive ethereum wallet address";
-            eth.TransferEthereum(toAddr, 0.05f).Wait();
+            //// Ethereum 전송 (toAddr에 전송할 Ethereum Wallet 주소 입력)
+            // 보낼 금액
+            //float amount = 0.5f;
+            //eth.TransferEthereum(toAddr, amount).Wait();
             #endregion
+
+            #region ERC-20 Token 전송 및 조회 샘플 코드
+            //// Token 잔액 조회
+            //eth.GetAccountTokenBalanceFromPrivateKey().Wait();
+
+            //// 보낼 토큰 액수 (Decimal 자리수까지 포함해야 합니다.)
+            //ulong amount = 30000;
+
+            //eth.TransferTokens(toAddr, amount).Wait();
+            #endregion
+
+            Console.WriteLine("Processes all over.");
             Console.ReadLine();
         }
 
+        #region Delegate Callback event
         /// <summary>
         /// Ethereum 송금이 완료 된 후 호출되는 Callback Event입니다. Transaction Hash를 출력합니다.
         /// </summary>
@@ -66,7 +89,7 @@ namespace EthereumBalance {
 
             System.Diagnostics.Process.Start("explorer.exe", string.Format("https://{0}etherscan.io/tx/{1}", server, Hash));
             #endregion
-
         }
+        #endregion
     }
 }
